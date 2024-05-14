99d432f268ec3832f871d72e6b44c02b8a573008
Damas2000 <Damas2000>
2024-05-15 02:26:19 +0300

    yeni

--- src/screens/CoursesInfo.js
+++ src/screens/CoursesInfo.js
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Information from '../../components/Information'
 
 export default function CoursesInfo() {
-  return (
-    <View>
-      <Text>CoursesInfo</Text>
-    </View>
-  )
+    return (
+        <ScrollView>
+
+            <Information title="React Native"
+                imageSource={require('../../assets/ReactNAtive.png')}
+                desc=" Kapsamlı React Native kursları"
+            />
+            <Information title="React JS"
+                imageSource={require('../../assets/ReactJS.png')}
+                desc=" Kapsamlı React JS kursları"
+            />
+
+            <Information title="Kotlin"
+                imageSource={require('../../assets/Kotlin.png')}
+                desc=" iş Tecrübesi"
+            />
+            <Information title="Fullstack Web Developer"
+                imageSource={require('../../assets/FullStack.png')}
+                desc=" iş tecrübesi"
+            />
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({})
